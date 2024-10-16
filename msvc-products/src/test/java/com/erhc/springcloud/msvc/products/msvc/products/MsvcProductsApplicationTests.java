e50668785ab385a6cbf72ac79752a318df74298b
Edwin HC <edwinrhc@gmail.com>
2024-10-15 23:32:45 -0500

    Msvc Products

---
+++ msvc-products/src/test/java/com/erhc/springcloud/msvc/products/msvc/products/MsvcProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erhc.springcloud.msvc.products.msvc.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
